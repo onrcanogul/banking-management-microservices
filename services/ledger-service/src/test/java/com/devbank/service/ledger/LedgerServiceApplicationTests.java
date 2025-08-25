c48d107a6e30810909f95a55dbda12f40ce5e91d
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-08-25 22:41:30 +0300

    ledger service was created (empty)

---
+++ services/ledger-service/src/test/java/com/devbank/service/ledger/LedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbank.service.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
