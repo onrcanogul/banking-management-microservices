77a2ef9f5d62471100c99226bb2929f783299fa5
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-08-31 14:25:04 +0300

    consumer bug was fixed in ledger service

--- commons/common-messaging/src/main/java/com/template/messaging/base/wrapper/EventWrapper.java
+++ commons/common-messaging/src/main/java/com/template/messaging/base/wrapper/EventWrapper.java
@@ -1,6 +1,8 @@
 package com.template.messaging.base.wrapper;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.template.messaging.event.Event;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.Map;
@@ -10,6 +12,11 @@ public record EventWrapper<T extends Event> (
         String type,
         String source,
         Instant time,
+        @JsonTypeInfo(
+                use = JsonTypeInfo.Id.CLASS,
+                include = JsonTypeInfo.As.EXTERNAL_PROPERTY,
+                property = "type"
+        )
         T event,
         Map<String, String> headers
 ) {
--- services/ledger-service/src/main/java/com/devbank/service/ledger/LedgerServiceApplication.java
+++ services/ledger-service/src/main/java/com/devbank/service/ledger/LedgerServiceApplication.java
@@ -1,13 +1,25 @@
 package com.devbank.service.ledger;
 
+import com.template.starter.outbox.StarterMarker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {
+		LedgerServiceApplication.class,
+		StarterMarker.class
+})
+@EnableJpaRepositories(basePackageClasses = {
+		LedgerServiceApplication.class,
+		StarterMarker.class
+})
+@EntityScan(basePackageClasses = {
+		LedgerServiceApplication.class,
+		StarterMarker.class
+})
 public class LedgerServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LedgerServiceApplication.class, args);
 	}
-
 }
